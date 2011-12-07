10793eb6b0055a1fdf17b2b24c8d64708ef00ee8
Choonghwan Lee <linjus.yi@gmail.com>
2011-12-07 11:16:41 -0600

    made InputStream_ManipulateAfterClose more precise

--- examples/java/io/InputStream_ManipulateAfterClose/InputStream_ManipulateAfterClose_1.java
+++ examples/java/io/InputStream_ManipulateAfterClose/InputStream_ManipulateAfterClose_1.java
@@ -2,8 +2,13 @@ import java.io.*;
 
 public class InputStream_ManipulateAfterClose_1 {
 	public static void main(String[] args) throws IOException {
-		byte[] buffer = { 1, 2, 3, 4, 5};
-		ByteArrayInputStream input = new ByteArrayInputStream(buffer);
+		File file = File.createTempFile("javamoptest1", ".tmp");
+		FileWriter writer = new FileWriter(file);
+		writer.write("0123456789");
+		writer.close();
+		file.deleteOnExit();
+
+		InputStream input = new FileInputStream(file);
 		int i = input.read();
 		input.close();
 
