3747768f44b47713c2d4a5edff13e786f973c90c
Choonghwan Lee <clee83@cs.uiuc.edu>
2011-12-12 22:06:54 -0600

    prevented InputStream_ReadAheadLimit and Reader_ReadAheadLimit from emitting spurious goodreset events

--- examples/java/io/Reader_ReadAheadLimit/BufferedReaderTest.java
+++ examples/java/io/Reader_ReadAheadLimit/BufferedReaderTest.java
@@ -6,10 +6,16 @@ public class BufferedReaderTest {
 		Reader reader = new BufferedReader(new StringReader("1234567890"), 1);
 
 		reader.mark(1);
-		reader.read();
-		reader.read();
 
+		// We read only one byte; so, reset() is valid.
+		reader.read();
 		reader.reset();
+
+		// The buffer cannot hold two bytes; so, reset() is invalid.
+		reader.read();
+		reader.read();
+		reader.reset();
+
 		reader.read();
 		reader.read();
 	}
